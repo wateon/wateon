03d7125f8611d681bb83d9e86e091a1339499ced
whChoi83 <whChoi83@89bc45da-3a20-11de-b418-9b7e54b75326>
2009-06-05 13:18:07 +0000

    Issue 4:  	 그룹 관리
     약간 손만 보면 됨. 거의 다 해감.

---
+++ trunk/WebContent/js/group.js
@@ -0,0 +1,23 @@
+// 그룹.
+function createGroup() {
+	var groupName = prompt("그룹명을 입력하세요", "");
+		
+	if (groupName)
+		$.post("group.do", {groupName: groupName, action: "create"});
+	else
+		alert("그룹명을 입력하세요");
+}
+
+function deleteGroup(groupName) {
+	var answer = confirm("삭제하시겠습니까?");
+	if (answer)
+		$.post("group.do", {groupName: groupName, action: "delete"});
+}
+
+function modifyGroup(groupName) {
+	var modifyGroupName = prompt("그룹명을 입력하세요", "");
+	if (modifyGroupName)
+		$.post("group.do", {groupName: groupName, modifyGroupName: modifyGroupName, action: "modify"});
+	else
+		alert("그룹명을 입력하세요");
+}
