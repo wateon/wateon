b4c48463dab19f6eae9d364c33068c3bd89a8251
ryunny <ryunny@89bc45da-3a20-11de-b418-9b7e54b75326>
2009-06-07 15:50:04 +0000

    친구 추가/삭제 대충.

--- trunk/src/wateon/servlet/FriendManagementServlet.java
+++ trunk/src/wateon/servlet/FriendManagementServlet.java
@@ -6,6 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kfmes.natelib.SwitchBoardSession;
+import kfmes.natelib.entity.FriendList;
+import kfmes.natelib.entity.NateFriend;
+
+import wateon.WateOn;
+import wateon.WateOnUser;
+
 public class FriendManagementServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -21,8 +28,27 @@ public class FriendManagementServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		
-		// TODO Auto-generated method stub
+		String action = request.getParameter("action");
+		String friendId = request.getParameter("friendId");
+		WateOnUser myself = WateOn.getInstance().getWateOnUser((String)request.getSession().getAttribute("id"));
+
 		
+		if (action.equals("add")) {
+			myself.getNateonMessenger().getNS().requestAdd(friendId, friendId +"님이 친구를 요청하셨습니다.");
+		}
+		else if (action.equals("delete")) {
+			NateFriend friend = new NateFriend(friendId);
+			myself.getNateonMessenger().getNS().removeBuddy(friend);
+		}
+		else if (action.equals("ban")) {
+			NateFriend friend = new NateFriend(friendId);
+			friend.setFABR(friendId);
+			friend.setUid(friendId);
+			myself.getNateonMessenger().getNS().sendBlock(friend, false);
+		}
+		else if (action.equals("unban")) {
+		}
+			// TODO Auto-generated method stub
 	}
 
 }
