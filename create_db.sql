2381803c4524bbf7fa01b30637d7fa40eb07ae67
jong10 <jong10@gmail.com>
2009-06-20 05:11:12 +0000

    DB 생성/삭제 SQL 문장 추가했음. wateon 루트 폴더에서,
    mysql -uroot -p < create_db.sql
    
    ..을 실행하면, DB가 생성되고, 유저가 생성됨.
    
    DB이름: wateon
    아이디: wateon
    패스워드: dnpdlxmdhs (웨이트온)
    
    mysql -uroot -p < drop_db.sql
    
    ..로 하면, 유저가 삭제되고, DB가 삭제됨.

---
+++ create_db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE wateon CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL ON wateon.* TO 'wateon'@'localhost' IDENTIFIED BY 'dnpdlxmdhs';
---
+++ drop_db.sql
@@ -0,0 +1,4 @@
+use mysql;
+DELETE FROM user WHERE user='wateon';
+DELETE FROM db WHERE user='wateon';
+DROP DATABASE wateon;
