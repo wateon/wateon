67ea50585d3e5c0091c2db0eaa524b3dd5432ad2
jong10 <jong10@89bc45da-3a20-11de-b418-9b7e54b75326>
2009-06-21 17:10:38 +0000

    Base64 인코더가 서버에서 문제가 생겨서, 다시 다른 클래스로 수정했음.

--- trunk/src/wateon/WateonUtil.java
+++ trunk/src/wateon/WateonUtil.java
@@ -1,32 +1,122 @@
 package wateon;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-
-import sun.misc.BASE64Encoder;
-
-class Base64 {
-	public static String encode(byte[] encodeBytes) {
-		byte[] buf = null;
-		String strResult = null;
-
-		BASE64Encoder base64Encoder = new BASE64Encoder();
-		ByteArrayInputStream bin = new ByteArrayInputStream(encodeBytes);
-		ByteArrayOutputStream bout = new ByteArrayOutputStream();
-
-		try {
-			base64Encoder.encodeBuffer(bin, bout);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		buf = bout.toByteArray();
-		strResult = new String(buf).trim();
-		return strResult;
-	}
-}
 
 public class WateonUtil {
 	public static String generateGroupId(String input) {
-		return Base64.encode(input.getBytes()).replace("=", "").replace("/", "");
+		return Base64Coder.encodeString(input);
 	}
 }
+
+
+/**
+ * A Base64 Encoder/Decoder.
+ * 
+ * <p>
+ * This class is used to encode and decode data in Base64 format as described in
+ * RFC 1521.
+ * 
+ * <p>
+ * This is "Open Source" software and released under the <a
+ * href="http://www.gnu.org/licenses/lgpl.html">GNU/LGPL</a> license.<br>
+ * It is provided "as is" without warranty of any kind.<br>
+ * Copyright 2003: Christian d'Heureuse, Inventec Informatik AG, Switzerland.<br>
+ * Home page: <a href="http://www.source-code.biz">www.source-code.biz</a><br>
+ * 
+ * <p>
+ * Version history:<br>
+ * 2003-07-22 Christian d'Heureuse (chdh): Module created.<br>
+ * 2005-08-11 chdh: Lincense changed from GPL to LGPL.<br>
+ * 2006-11-21 chdh:<br>
+ * &nbsp; Method encode(String) renamed to encodeString(String).<br>
+ * &nbsp; Method decode(String) renamed to decodeString(String).<br>
+ * &nbsp; New method encode(byte[],int) added.<br>
+ * &nbsp; New method decode(String) added.<br>
+ */
+class Base64Coder {
+
+	// Mapping table from 6-bit nibbles to Base64 characters.
+	private static char[] map1 = new char[64];
+	static {
+		int i = 0;
+		for (char c = 'A'; c <= 'Z'; c++)
+			map1[i++] = c;
+		for (char c = 'a'; c <= 'z'; c++)
+			map1[i++] = c;
+		for (char c = '0'; c <= '9'; c++)
+			map1[i++] = c;
+		map1[i++] = '-';	// + -> - for url safe
+		map1[i++] = '_';	// / -> _ for url safe
+	}
+
+	// Mapping table from Base64 characters to 6-bit nibbles.
+	private static byte[] map2 = new byte[128];
+	static {
+		for (int i = 0; i < map2.length; i++)
+			map2[i] = -1;
+		for (int i = 0; i < 64; i++)
+			map2[map1[i]] = (byte) i;
+	}
+
+	/**
+	 * Encodes a string into Base64 format. No blanks or line breaks are
+	 * inserted.
+	 * 
+	 * @param s
+	 *            a String to be encoded.
+	 * @return A String with the Base64 encoded data.
+	 */
+	public static String encodeString(String s) {
+		return new String(encode(s.getBytes()));
+	}
+
+	/**
+	 * Encodes a byte array into Base64 format. No blanks or line breaks are
+	 * inserted.
+	 * 
+	 * @param in
+	 *            an array containing the data bytes to be encoded.
+	 * @return A character array with the Base64 encoded data.
+	 */
+	public static char[] encode(byte[] in) {
+		return encode(in, in.length);
+	}
+
+	/**
+	 * Encodes a byte array into Base64 format. No blanks or line breaks are
+	 * inserted.
+	 * 
+	 * @param in
+	 *            an array containing the data bytes to be encoded.
+	 * @param iLen
+	 *            number of bytes to process in <code>in</code>.
+	 * @return A character array with the Base64 encoded data.
+	 */
+	public static char[] encode(byte[] in, int iLen) {
+		int oDataLen = (iLen * 4 + 2) / 3; // output length without padding
+		int oLen = ((iLen + 2) / 3) * 4; // output length including padding
+		char[] out = new char[oLen];
+		int ip = 0;
+		int op = 0;
+		while (ip < iLen) {
+			int i0 = in[ip++] & 0xff;
+			int i1 = ip < iLen ? in[ip++] & 0xff : 0;
+			int i2 = ip < iLen ? in[ip++] & 0xff : 0;
+			int o0 = i0 >>> 2;
+			int o1 = ((i0 & 3) << 4) | (i1 >>> 4);
+			int o2 = ((i1 & 0xf) << 2) | (i2 >>> 6);
+			int o3 = i2 & 0x3F;
+			out[op++] = map1[o0];
+			out[op++] = map1[o1];
+			out[op] = op < oDataLen ? map1[o2] : '_';
+			op++;
+			out[op] = op < oDataLen ? map1[o3] : '_';
+			op++;
+		}
+		return out;
+	}
+
+	// Dummy constructor.
+	private Base64Coder() {
+	}
+
+} // end class Base64Coder
